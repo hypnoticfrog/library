23dead30e4964d8c75740ad38dedd4dd4697d47e
hypnoticfrog <andymohan1@hotmail.com>
2016-04-25 09:20:59 -0400

    Added files via upload

---
+++ Book.java
@@ -0,0 +1,41 @@
+package chapterninepointproject;
+
+public class Book {
+	
+	private String title;
+	private String author;
+	private int year;
+	
+	public Book(String title, String author, int year) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+	public int getYear() {
+		return year;
+	}
+	
+	
+
+}
